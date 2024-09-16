8c4a6f9fead1a47ec4e19ca2adc998463af7f75b
Lloffert <sailorlloffert@gmail.com>
2024-09-16 11:50:33 -0400

    Bookmark

---
+++ P1HW1_LoffertLydia.py
@@ -0,0 +1,13 @@
+#Lydia Loffert
+#9/16/2024
+#P1HW1
+#Collect info from the user, processes info collected and display results
+
+print("-----Calculating Exponents-----")
+
+base_value= int(input("Enter an integer as the base value: "))
+
+exponent= int(input("Enter an integer as the exponent: "))
+
+#Display result to user
+print(base_value, "rasied to the power of", exponent, "is", base_value**exponent)
