72c319ec251353c3fe4e287c2cf1c62e8baaff98
Lloffert <sailorlloffert@gmail.com>
2024-10-14 11:42:42 -0400

    Bookmark unfinished
    work on later

---
+++ P3LAB_LoffertLydia.py
@@ -0,0 +1,16 @@
+# Lydia Loffert
+# 10/14/2024
+# P3LAB
+# Calculate the most efficient number of change needed
+
+#Regular division
+print(100/3)
+
+#Floor division - returns ONLY integer portion of quotient
+print(100//3)
+
+#Modulo - returns ONLY the remainder
+print(100%3)
+print(7%4)
+
+#input("Enter the amount of money as a float: ")
