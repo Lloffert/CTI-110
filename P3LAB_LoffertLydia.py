6b9b4b77d3180d17adf30a1050558cc73cba8230
Lloffert <sailorlloffert@gmail.com>
2024-10-21 11:46:59 -0400

    10/21 Latest bookmark WIP

--- P3LAB_LoffertLydia.py
+++ P3LAB_LoffertLydia.py
@@ -2,7 +2,7 @@
 # 10/14/2024
 # P3LAB
 # Calculate the most efficient number of change needed
-
+'''
 #Regular division
 print(100/3)
 
@@ -12,5 +12,43 @@ print(100//3)
 #Modulo - returns ONLY the remainder
 print(100%3)
 print(7%4)
+'''
+# Get amount of money from user as a float
+money = float(input("Enter the amount of money as a float: $"))
 
-#input("Enter the amount of money as a float: ")
+# Convert money to a whole number
+money = round(money * 100)
+
+# May need to round this value
+# money = int(money * 100)
+
+# Get whole dollars from money amount
+dollars = money // 100
+print(f"{dollars} Dollars")
+# Remove dollar amount from money value
+money = money - (dollars * 100)
+
+# Get quarters from money amount
+quarters = money // 25
+
+# Remove quarter amount from money value
+money = money - (quarters * 25)
+print(f"{quarters} Quarters")
+
+# Get dimes from money amount
+dimes = money // 10
+
+# Remove dime amount from money value
+money = money - (dimes * 10)
+print(f"{dimes} Dimes")
+
+# Get nickels from money amount
+nickels = money // 5
+
+# Remove nickel amount from money value
+money = money - (nickels * 5)
+print(f"{nickels} Nickels")
+
+# Get pennies from money amount
+pennies = money
+print(f"{pennies} Pennies")
